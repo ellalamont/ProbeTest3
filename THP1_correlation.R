29d9c685da357285336e2f93263936b0b0858186
Ella Lamont <elamont@uw.edu>
2025-01-25 14:13:37 -0800

    Changing more PCA plots

--- THP1_correlation.R
+++ THP1_correlation.R
@@ -20,32 +20,30 @@ my_plot_themes <- theme_bw() +
   theme(legend.position = "right",legend.text=element_text(size=10),
         legend.title = element_text(size = 10),
         plot.title = element_text(size=10), 
-        axis.title.x = element_text(size=14), 
-        axis.text.x = element_text(angle = 0, size=14, vjust=0, hjust=0.5),
-        axis.title.y = element_text(size=14),
-        axis.text.y = element_text(size=14), 
-        plot.subtitle = element_text(size=9), 
-        plot.margin = margin(10, 10, 10, 20),
-        panel.background = element_rect(fill='transparent'),
-        plot.background = element_rect(fill='transparent', color=NA),
-        legend.background = element_rect(fill='transparent'),
-        legend.box.background = element_blank()
-  )
+        axis.title.x = element_blank(), 
+        axis.text.x = element_text(angle = 90, size=10, vjust=0, hjust=0.5),
+        axis.title.y = element_blank(),
+        axis.text.y = element_text(size=10), 
+        plot.subtitle = element_text(size=10), 
+        plot.margin = margin(10, 10, 10, 20))
 
 
 ###########################################################
 ##################### LOG10 GGCORRPLOT ####################
 # Only going to do Pearson because the number of genes is so high CLT applies and parametric tests can be used
 
+# remove the Euk dep samples
+my_THP1_tpm_Log10_2 <- my_THP1_tpm_Log10[, !grepl("Euk", colnames(my_THP1_tpm_Log10))]
+
 # Make the correlation
-my_cor_pearson <- cor(my_THP1_tpm_Log10, method = "pearson")
+my_cor_pearson <- cor(my_THP1_tpm_Log10_2, method = "pearson")
 
 min(my_cor_pearson) # 0.3743603
 
 # Plot pearson
 Corr_plot <- my_cor_pearson %>% 
   ggcorrplot(hc.order = FALSE, 
-             lab = TRUE, lab_size = 1,
+             lab = TRUE, lab_size = 1.5,
              type = c("full")) + 
   scale_fill_gradient2(limit = c(0.37,1), low = "blue", high =  "red", mid = "white", midpoint = 0.68) + # Make sure to change based on the min!
   my_plot_themes + 
@@ -56,9 +54,9 @@ Corr_plot <- my_cor_pearson %>%
 Corr_plot
 
 ggsave(Corr_plot,
-       file = "AllTHP1_PearsonLog10.pdf",
+       file = "AllrRNATHP1_PearsonLog10.pdf",
        path = "Correlation_Figures",
-       width = 7, height = 6, units = "in")
+       width = 6, height = 6, units = "in")
 
 
 
